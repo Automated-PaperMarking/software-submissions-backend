d60d118630948123c1bbb75b28352ad99c40356e
Shenith404 <shenithbandara@gmail.com>
2025-09-10 16:15:31 +0530

    updated

--- backend/src/run/run.worker.ts
+++ backend/src/run/run.worker.ts
@@ -2,7 +2,7 @@ import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { AI_GRADING_QUEUE } from 'src/config/config';
 
-@Processor(AI_GRADING_QUEUE)
+@Processor(AI_GRADING_QUEUE,{ concurrency: 5 })
 export class RunWorker extends WorkerHost {
   async process(job: Job) {
     console.log('Processing job:', job.id, job.data);
